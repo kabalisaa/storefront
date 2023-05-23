5397f02edc7068c4462f7a6b737b52d6ec3b85b8
kabalisa <71097638+Nesty1550@users.noreply.github.com>
2023-05-23 22:27:49 +0200

    creation of all necessary models

--- playground/views.py
+++ playground/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def say_hello(request):
-    return HttpResponse("hello world!")
+    return render(request, 'index.html',) #mapping  views with templates
+    #return HttpResponse("hello world!") #mapping only views 
